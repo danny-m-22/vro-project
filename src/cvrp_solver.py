d9b153f4d761cdedaa9f5362ec8b28caec01f335
DannyMarren <212231967+danny-m-22@users.noreply.github.com>
2025-06-22 09:48:43 -0400

    NN solver done

--- src/cvrp_solver.py
+++ src/cvrp_solver.py
@@ -1,6 +1,7 @@
 import openrouteservice
 import os
 import pandas as pd
+import numpy as np
 from src.config import ORS_API_KEY
 
 base_dir = os.path.dirname(os.path.dirname(__file__))
@@ -9,62 +10,52 @@ input_file = os.path.join(base_dir, "data", "durations.csv")
 
 duration_df = pd.read_csv(input_file)
 duration_df = duration_df.drop(columns="Unnamed: 0")
-
-# Path Characteristics
-path_length = len(duration_df)
+duration_df = duration_df.replace(0, np.nan)   # crucial for min calculations
 
 # BEST PATH
-best_time = 0
-best_start_node = 0
-best_list = [0,1,2,3,4,5,6,7,8,9]
+best_dist = 0
+best_list = []
 
 # CURRENT PATH VARIABLES
 visited_list = []
-not_visited_list = [0,1,2,3,4,5,6,7,8,9]
-current_time = 0
 
-print(duration_df)
 
-for starting_loc in range(path_length):
-    visited_list.append(starting_loc)
-    not_visited_list.remove(starting_loc)
+# NN SOLVER
+    #START AT EACH ADDRESS AND FIND SHORTEST PATH; SAVE BEST PATH
+    #REMEMBER: FROM-TO MATRIX; ROW TO COL
 
-    # NN IMPLEMENTATION & TIME TRACKER
-    while not_visited_list:
-        min_index = duration_df[str(starting_loc)].idxmin(axis=0)  # get location of min
-        print(min_index)
-        current_time += min(duration_df.iloc[not_visited_list, starting_loc])  # add min to current time
-        not_visited_list.remove(min_index)  # remove address from not visited
-        visited_list.append(min_index)      # add new address
+for loc in range(len(duration_df)):
+    current_loc = loc
+    duration_copy = duration_df.copy()
+    visited_list.append(current_loc)
+    current_dist = 0
 
-    current_time += duration_df.iloc[visited_list[0], visited_list[0]]  # get end to start value
+    # NN IMPLEMENTATION & DISTANCE TRACKER
+    while len(visited_list) < len(duration_df):
+
+        duration_copy = duration_copy.drop(str(current_loc), axis=1)
+        min_index = duration_copy.loc[int(current_loc)].idxmin()  # get location of min
+        current_dist += duration_copy.loc[int(current_loc), str(min_index)]  # add min to current time
+        #print(f"{duration_copy.loc[current_loc, str(min_index)]} from {current_loc} to {min_index}")
+        current_loc = int(min_index)             # update current_loc
+        visited_list.append(int(min_index))      # add new address
+
+    current_dist += duration_df.iloc[current_loc, loc]  # get end to start value
+    visited_list.append(loc)  # return to start (optional to include in list)
+
+    # print(f"{duration_df.iloc[current_loc, loc]} from {current_loc} to {loc}")
+    # print(f"total: {current_time}, order: {visited_list}")
+    # print("\n\n")
 
     # IF NEW BEST
-    if current_time < best_time  or best_time == 0:
-      best_time = current_time
+    if current_dist < best_dist  or best_dist == 0:
+      best_dist = current_dist
       best_list = list(visited_list)
 
     # RESET VARS
-    current_time = 0
     visited_list.clear()
-    not_visited_list = [0,1,2,3,4,5,6,7,8,9]
-
-print(best_list)
-
-   # i = 0
-    # while i+1 < path_length:
-    #   current_time += duration_df.iloc[visited_list[i], visited_list[i+1]]
-    #   i+= 1
-    # current_time += duration_df.iloc[visited_list[i], visited_list[0]]
 
 
-# PLAN:
-# Nearest neighbor approach (NN)
-# Start at 0
-# Go to next CLOSEST node
-# Get back to 0
-# Iterate: start at another node (MNN)
-# overwrite results if newest tour is better
 # Attempt 2-opt (or k-opt) to smooth kinks
 # NN + MNN
 
